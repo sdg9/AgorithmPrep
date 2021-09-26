03481a52ff71d62766c9332ff09a1fe022650edf
Steven Goff <steveng9@gmail.com>
2021-09-25 22:41:56 -0500

    implement binary search

---
+++ src/algorithms/__tests__/binarySearch-test.ts
@@ -0,0 +1,10 @@
+import { binarySearch } from '../binarySearch';
+
+const array = [1, 2, 3, 5, 7, 9, 11];
+it('should find valid result', () => {
+  expect(binarySearch(array, 7)).toBe(true);
+});
+
+it('should not find invalid result', () => {
+  expect(binarySearch(array, 8)).toBe(false);
+});
--- src/algorithms/binarySearch.ts
+++ src/algorithms/binarySearch.ts
@@ -1,2 +1,22 @@
-console.log('Hello');
-const input = [1, 3, 5, 7, 9, 11];
+export function binarySearch(
+  array: number[],
+  goal: number,
+  start: number = 0,
+  end: number = array.length
+) {
+  if (start > end) {
+    return false;
+  }
+
+  let middle = Math.floor((start + end) / 2);
+
+  if (array[middle] === goal) {
+    return true;
+  }
+
+  if (array[middle] > goal) {
+    return binarySearch(array, goal, start, middle - 1);
+  } else {
+    return binarySearch(array, goal, middle + 1, end);
+  }
+}
