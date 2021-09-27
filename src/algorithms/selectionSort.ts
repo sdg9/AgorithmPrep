9ca12c062cd5ec73ad391f1cb2ee17ec0e48c939
Steven Goff <steveng9@gmail.com>
2021-09-26 19:59:01 -0500

    Implement selection sort

--- src/algorithms/selectionSort.ts
+++ src/algorithms/selectionSort.ts
@@ -1,3 +1,19 @@
+/**
+ * Sorts by finding minimum element from unsorted list and putting it at the beginning.
+ * Time Complexity: O(n^2)
+ * @param array
+ * @returns
+ */
 export function selectionSort(array: number[]) {
-    return [];
+  for (let i = 0; i < array.length; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIdx]) {
+        minIdx = j;
+      }
+
+      [array[minIdx], array[i]] = [array[i], array[minIdx]];
+    }
+  }
+  return array;
 }
