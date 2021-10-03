1c228c3ffeff74677d3c150ebfb3518726d8ee41
Steven Goff <steveng9@gmail.com>
2021-10-03 12:37:47 -0500

    WIP

--- src/facebook/codingPuzzles/08-DirectorOfPhotography2.ts
+++ src/facebook/codingPuzzles/08-DirectorOfPhotography2.ts
@@ -12,13 +12,18 @@
 
 export function getArtisticPhotographCount(N, C, X, Y) {
   // Write your code here
-  return (
-    getArtisticHelper(N, C, X, Y) +
-    getArtisticHelper(N, C.split('').reverse().join(''), X, Y)
-  );
+  return getArtisticHelper2(N, C, X, Y);
+  // getArtisticHelper(N, C.split('').reverse().join(''), X, Y)
 }
 
-// You solved 33 / 39 test cases
+/**
+ * You solved 33 / 39 test cases
+ * @param N
+ * @param C
+ * @param X
+ * @param Y
+ * @returns
+ */
 function getArtisticHelper(N, C, X, Y) {
   const length = N;
   const array = C;
@@ -31,16 +36,65 @@ function getArtisticHelper(N, C, X, Y) {
   // ..B.AP..
   let artistic = 0;
 
-  for (let p = 0; p < array.length; p++) {
-    if (array[p] === 'P') {
-      for (let a = p + X; a < array.length && a - p <= Y; a++) {
+  for (let pb = 0; pb < array.length; pb++) {
+    if (array[pb] === 'P' || array[pb] == 'B') {
+      const other = array[pb] === 'P' ? 'B' : 'P';
+      for (let a = pb + X; a < array.length && a - pb <= Y; a++) {
         if (array[a] === 'A') {
           for (let b = a + X; b < array.length && b - a <= Y; b++) {
-            if (array[b] === 'B') {
+            if (array[b] === other) {
               artistic += 1;
             }
           }
         }
+      }
+    }
+  }
+
+  return artistic;
+}
+
+function between(x, min, max) {
+  return min <= x && x <= max;
+}
+
+/**
+ * You solved 15 / 39 test cases
+ * @param N
+ * @param C
+ * @param X
+ * @param Y
+ * @returns
+ */
+function getArtisticHelper2(N, C, X, Y) {
+  const length = N;
+  const array = C;
+  let artistic = 0;
+
+  let pSet = new Set<number>();
+  let aSet = new Set<number>();
+  let bSet = new Set<number>();
+
+  // O(n)
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'P') {
+      pSet.add(i);
+    } else if (array[i] === 'A') {
+      aSet.add(i);
+    } else if (array[i] === 'B') {
+      bSet.add(i);
+    }
+  }
+
+  // O(n^3?)
+  for (let p of pSet) {
+    for (let a of aSet) {
+      for (let b of bSet) {
+        const PA = Math.abs(p - a);
+        const BA = Math.abs(b - a);
+        const PBOppositeA = (p < a && a < b) || (b < a && a < p);
+        artistic +=
+          PBOppositeA && between(PA, X, Y) && between(BA, X, Y) ? 1 : 0;
       }
     }
   }
