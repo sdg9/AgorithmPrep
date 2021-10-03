f8372b4d88aa0abd857297e6e2455bc3b49f8035
Steven Goff <steveng9@gmail.com>
2021-10-03 13:58:40 -0500

    WIP

--- src/facebook/codingPuzzles/09-Hops.ts
+++ src/facebook/codingPuzzles/09-Hops.ts
@@ -6,5 +6,17 @@
  */
 export function getSecondsRequired(N, F, P) {
   // Write your code here
-  return 0;
+  const sortedLilyPads = P.slice(0).sort((a, b) => b - a); // O(n log n)
+  let hops = 0;
+  for (let i = 0; i < P.length; i++) {
+    hops += N - P[i] - i;
+  }
+  return hops;
+
+  // return P.slice(0)
+  //   .sort((a, b) => b - a) // O(n log n)
+  //   .reduce((prev, curr, idx) => {
+  //     // O(n)
+  //     return prev + N - curr - idx;
+  //   }, 0);
 }
--- src/facebook/codingPuzzles/__tests__/09-Hops-test.ts
+++ src/facebook/codingPuzzles/__tests__/09-Hops-test.ts
@@ -6,4 +6,7 @@ describe('09-Hops', () => {
   it('should work as intended case 2', () => {
     expect(getSecondsRequired(6, 3, [5, 2, 4])).toBe(4);
   });
+  it('should work as intended custom test case 3', () => {
+    expect(getSecondsRequired(12, 4, [5, 1, 10, 3])).toBe(23);
+  });
 });
